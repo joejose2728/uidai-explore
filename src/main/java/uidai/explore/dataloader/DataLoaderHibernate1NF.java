492e066ad914ac62d815224ac9337ec6186bdd58
vijayakedar <vijayakedar@gmail.com>
2014-10-25 17:45:08 -0700

    commit 1 NF - hibernate

---
+++ src/main/java/uidai/explore/dataloader/DataLoaderHibernate1NF.java
@@ -0,0 +1,89 @@
+package uidai.explore.dataloader;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import uidai.explore.hibernate.nf1.data.AadharRecord;
+import uidai.explore.hibernate.nf1.util.HibernateUtil;
+import uidai.explore.intf.IDataLoader;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class DataLoaderHibernate1NF implements IDataLoader{
+
+	public void loadUidaiData(List<String[]> csvValues, String enrollmentDate) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		
+		final int batchSize = 1000;
+		int cnt =0;
+		Date date = null;
+		
+		try {
+			date = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).parse(enrollmentDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			 Transaction tx = session.beginTransaction();
+			 for (String[] row: csvValues)
+			 {
+				 AadharRecord record = new AadharRecord();
+				 record.setRegistrar(row[0]);//registrar
+				 record.setEnrollmentAgency(row[1]);//ea
+				 record.setState(row[2]);//state
+				 record.setDistrict(row[3]);//district
+				 record.setSubDistrict(row[4]);//sub-district
+					long pin_code = 000000;
+					try{
+						pin_code = Long.parseLong(row[5].trim());
+					}catch(NumberFormatException e){
+					}
+					
+				record.setPinCode(pin_code);//pin code
+				record.setGender(row[6].toCharArray()[0]);//gender
+				record.setAge(Integer.parseInt(row[7]));//age
+				record.setAadharGenerated(parseColumn(row[8]));//aadhaar generated?
+				record.setEnrollmentRejected(parseColumn(row[9]));//enrollment rejected?
+				record.setEmailProvided(parseColumn(row[10]));//has email
+				record.setPhoneNumberProvided(parseColumn(row[11]));//has phone
+				record.setEnrollmentDate(date);
+				
+				session.save(record);
+					
+					if (++cnt % batchSize == 0)
+					{			 
+						session.flush();
+						session.clear();
+					}
+		
+			}
+			tx.commit();
+		} finally {
+			// session.close();
+		}
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private int parseColumn(String col){
+		int val = 0;
+		try{
+			val = Integer.parseInt(col.trim());
+		}catch (NumberFormatException e){
+		}
+		return val;
+	}
+
+	
+	
+
+}
---
+++ src/main/java/uidai/explore/hibernate/nf1/data/AadharRecord.java
@@ -0,0 +1,116 @@
+package uidai.explore.hibernate.nf1.data;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "aadhar_record")
+public class AadharRecord {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="aid", updatable=false, nullable= false)
+	private long aid;
+	private String registrar;
+	private String enrollmentAgency;
+	private String state;
+	private String district;
+	private String subDistrict;
+	private long pinCode;
+	private char gender;
+	private int age;
+	private int aadharGenerated;
+	private int enrollmentRejected;
+	private int emailProvided;
+	private int phoneNumberProvided;
+	private Date enrollmentDate;
+	
+	
+	public long getAid() {
+		return aid;
+	}
+	public void setAid(long aid) {
+		this.aid = aid;
+	}
+	public String getRegistrar() {
+		return registrar;
+	}
+	public void setRegistrar(String registrar) {
+		this.registrar = registrar;
+	}
+	public String getEnrollmentAgency() {
+		return enrollmentAgency;
+	}
+	public void setEnrollmentAgency(String enrollmentAgency) {
+		this.enrollmentAgency = enrollmentAgency;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getSubDistrict() {
+		return subDistrict;
+	}
+	public void setSubDistrict(String subDistrict) {
+		this.subDistrict = subDistrict;
+	}
+	public long getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(long pinCode) {
+		this.pinCode = pinCode;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getAadharGenerated() {
+		return aadharGenerated;
+	}
+	public void setAadharGenerated(int aadharGenerated) {
+		this.aadharGenerated = aadharGenerated;
+	}
+	public int getEnrollmentRejected() {
+		return enrollmentRejected;
+	}
+	public void setEnrollmentRejected(int enrollmentRejected) {
+		this.enrollmentRejected = enrollmentRejected;
+	}
+	public int getEmailProvided() {
+		return emailProvided;
+	}
+	public void setEmailProvided(int emailProvided) {
+		this.emailProvided = emailProvided;
+	}
+	public int getPhoneNumberProvided() {
+		return phoneNumberProvided;
+	}
+	public void setPhoneNumberProvided(int phoneNumberProvided) {
+		this.phoneNumberProvided = phoneNumberProvided;
+	}
+	public Date getEnrollmentDate() {
+		return enrollmentDate;
+	}
+	public void setEnrollmentDate(Date enrollmentDate) {
+		this.enrollmentDate = enrollmentDate;
+	}
+	
+	
+}
